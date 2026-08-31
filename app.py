2f3290eebf5f4fda21f4c65e2ca6e29ca10b5e72
zirwa29 <zirwakhalid67@gmail.com>
2026-08-31 12:50:44 +0500

    Day 9 — Connect the Matcher to Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
 
 app = Flask(__name__)
@@ -12,9 +12,47 @@ def home():
 @app.route("/recommendations")
 def recommendations():
 
-    internships = pd.read_csv("data/recommended_internships.csv")
+    skills = request.args.get("skills")
 
-    return jsonify(internships.to_dict(orient="records"))
+    if not skills:
+        return jsonify({
+            "error": "Please provide skills"
+        }), 400
+
+    student_skills = [
+        skill.strip().lower()
+        for skill in skills.split(",")
+    ]
+
+    internships = pd.read_csv("data/internships.csv")
+
+    def calculate_match(internship_skills):
+
+        internship_skills = [
+            skill.strip().lower()
+            for skill in internship_skills.split(",")
+        ]
+
+        matched_skills = set(student_skills) & set(internship_skills)
+
+        return len(matched_skills)
+
+    internships["match_score"] = internships["skills"].apply(
+        calculate_match
+    )
+
+    internships = internships[internships["match_score"] > 0]
+
+    internships = internships.sort_values(
+        by="match_score",
+        ascending=False
+    )
+
+    top_internships = internships.head(5)
+
+    return jsonify(
+        top_internships.to_dict(orient="records")
+    )
 
 
 if __name__ == "__main__":
